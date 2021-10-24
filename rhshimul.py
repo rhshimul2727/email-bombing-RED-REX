028ff1cdf373f5f2b2c2faa0660c0c2335a6000f
rhshimul2727 <92758735+rhshimul2727@users.noreply.github.com>
2021-10-25 05:25:06 +0600

    Update rhshimul.py

--- rhshimul.py
+++ rhshimul.py
@@ -4,4 +4,9 @@ server.starttls()
 server.login("doubleaech1@gmail.com","11223344$")
 for i in range(50):
     server.sendmail("yourmaul@gmail.com","doubleaech3@gmail.com","Tor mare chudi")
+    print 'email = raw_input('Attacker Gmail Address : ')
+   
     print 'massage has been send'
+
+
+    print 'REDREX-----########'
